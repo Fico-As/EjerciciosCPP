738af6ec68ee13d433464e711eefce11a4ac0de4
Fico-As <fidelcrnj@gmail.com>
2021-04-04 10:37:24 -0400

    primero CPP

---
+++ holaClasico.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include<ncurses.h>
+void main(){
+    printf("Hola Clasico Ncurses \n");
+    //getch();
+}
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hola mundo LINUX"<<endl;
+    return 0;
+}
